b9b980e2f041d4bb3b4279b90aeb3839d662d27b
HueJazz <alexkorystin@ukr.net>
2022-10-26 20:33:15 +0300

    lab2

--- calculator.cpp
+++ calculator.cpp
@@ -5,11 +5,19 @@ int Calculator::Add (double a, double b)
 <<<<<<< HEAD
     return a + b;
 =======
-	return a + b + 0.5;
+	return a + b;
 >>>>>>> 4ad40a3 (fix truncation error)
 }
 
 int Calculator::Sub (double a, double b)
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
 {
     return Add (a, -b);
 }
